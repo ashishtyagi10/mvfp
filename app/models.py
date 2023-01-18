9608a6992be678ef77399c8363ccc288125cc01e
Ashish Tyagi <ashishtyagi10@gmail.com>
2023-01-17 23:39:26 -0500

    added the model for drug that will appear on the website

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Drug(models.Model):
+    drug_name = models.CharField(max_length=200)
+    available_quantity = models.IntegerField(default=0)
+    unit_price = models.DecimalField(default=0.0)
+    pub_date = models.DateTimeField('Date Updated')
