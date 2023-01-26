75e840db21506d6aacecb4d7328674de00560da2
Ashish Tyagi <ashishtyagi10@gmail.com>
2023-01-26 10:26:28 -0500

    added part 3 of tutorial

--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
-
+# https://medium.com/analytics-vidhya/how-to-create-simple-e-commerce-website-with-django-step-1-of-5-42c6cca414c2
 # https://medium.com/analytics-vidhya/how-to-create-fully-functional-e-commerce-website-with-django-c95b46973b0
+# https://medium.com/analytics-vidhya/how-to-create-fully-functional-e-commerce-website-with-django-997ffaa0f040
+# https://medium.com/analytics-vidhya/how-to-create-fully-functional-e-commerce-website-with-django-7205d250e76f
+# https://medium.com/analytics-vidhya/how-to-create-a-fully-functional-e-commerce-website-with-django-d4b998bac01
+
 # Create your models here.
 
 
@@ -45,6 +49,20 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.quantity} of {self.item.item_name}"
 
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_discount_item_price(self):
+        return self.quantity * self.item.discount_price
+
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_discount_item_price()
+
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_discount_item_price()
+        return self.get_total_item_price()
+
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -56,3 +74,9 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_total_price(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import (
     remove_from_cart,
     add_to_cart,
+    reduce_quantity_item,
     ProductView,
-    HomeView
+    HomeView,
+    OrderSummaryView,
 )
 
 app_name = 'app'
@@ -12,5 +14,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<pk>/', ProductView.as_view(), name='product'),
     path('add-to-cart/<pk>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart'),
+    path('order-summary', OrderSummaryView.as_view(), name='order-summary'),
+    path('reduce-quantity-item/<pk>/',
+         reduce_quantity_item, name='reduce-quantity-item'),
 ]
